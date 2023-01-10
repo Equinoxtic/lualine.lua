7be90d6b34992ca813684bf8cfe7da2cdd3aed16
Lunaxtic <86795271+Equinoxtic@users.noreply.github.com>
2023-01-10 20:04:39 +0800

    Create lualine.lua

---
+++ lualine.lua
@@ -0,0 +1,157 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
+
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic", "coc" },
+	sections = { "error", "warn" },
+	symbols = { error = "err: ", warn = "warn: " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
+	fmt = function(str)
+		return "(" .. str .. ")"
+	end,
+}
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = '* ', modified = '+ ', removed = '~ ' }, -- changes diff symbols
+}
+
+local mode = {
+	"mode",
+	fmt = function(str)
+		return "--"..str.."--"  
+	end,
+}
+
+local filetype = {
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
+	fmt = function (str)
+		if not (str == nil or str == "") then
+			return "(." .. str .. " file)"
+		else
+			return "(Open a file with \":e\")"
+		end
+	end,
+}
+
+local branch = {
+	"branch",
+	icons_enabled = false,
+	fmt = function(str)
+		if (str == nil or str == "") then
+			return "*"..str
+		else
+			return "Git:"..str
+		end
+	end,
+}
+
+local location = {
+	"location",
+	fmt = function(str)
+		return "@:" .. str
+	end,
+}
+
+
+local spaces = function()
+	return "spcs: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+local filename = {
+	"filename",
+	symbols = {
+		modified = "*",
+		readonly = "!",
+		unnamed = "n",
+		newfile = "+",
+	},
+	path = 1,
+	fmt = function(str)
+		if not (str == "n") then
+			return "[~\\"..str.."]"
+		else
+			return "[No Name...]";
+		end
+	end,
+}
+
+local fileformat = {
+	"fileformat",
+	symbols = {
+		unix = "Unix (LF)",
+		dos = "Windows (CRLF)",
+		mac = "Unix (LF)",
+	},
+	fmt = function(str)
+		return "*"..str.."*"
+	end,
+}
+
+local progress = {
+	"progress",
+	fmt = function(str)
+		if not (str == "Top" or str == "Bot") then
+			return "("..str..")"
+		else
+			if (str == "Bot") then
+				return "(EOF / End Of File)"
+			elseif (str == "Top") then
+				return "(BOF / Beggining Of File)"
+			end
+		end
+	end,
+}
+
+--[[ local buffers = {
+	"buffers",
+	mode = 0,
+	show_modified_status = false,
+	fmt = function(str)
+		return "*"..str.."*"
+	symbols = {
+		modified = '*'
+	}
+} ]]
+
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { branch },
+		lualine_b = { mode },
+		lualine_c = { filename, filetype, progress },
+		lualine_x = { diagnostics },
+		lualine_y = { spaces, "encoding", fileformat },
+		-- lualine_y = {},
+		lualine_z = { location },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
